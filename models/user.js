3fa29b937c00ae8d8d273df551875a10943bf3b3
anggiela dorador <a.doradorchipana@gmail.com>
2021-03-13 18:08:54 -0300

    se muestra uid en vez de _id cuando se retornla al usuario

--- models/user.js
+++ models/user.js
@@ -5,16 +5,16 @@ const UserSchema = Schema({
   email: { type: String, unique: true },
   pass: { type: String, required: [true, "la contraseña es obligatoria"] },
   img: { type: String },
-  //rol: { type: String, required: true, emun: ["ADMIN_ROLE", "USER_ROLE"] },
-  estado: { type: Boolean, default: true },
+  role: { type: String, required: true, emun: ["ADMIN_ROLE", "USER_ROLE"], default:"USER_ROLE"},
+  isActive: { type: Boolean, default: true },
   google: { type: Boolean, default: false },
 });
 
 //rescribe metodo toJSON para no mostrar __v y pass
 //cuando se muestra la respuesta
 UserSchema.methods.toJSON = function () {
-  const { __v, pass,id,  ...user } = this.toObject();
-  user.uid = id //transforma en vista el id mostrandolo como uid
+  const { __v, pass, _id,  ...user } = this.toObject();
+  user.uid = _id //transforma en vista el id mostrandolo como uid
   return user;
 };
 
