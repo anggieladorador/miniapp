034472d93a7eaad7b18d498a6cb1eaffbeebb02e
anggiela dorador <a.doradorchipana@gmail.com>
2021-03-13 21:05:45 -0300

    se obtienen app por usuario

--- controller/app.js
+++ controller/app.js
@@ -1,13 +1,15 @@
+
 const App = require("../models/app")
+const ObjectId = require("mongoose").Types.ObjectId;
 
 
-postApp  =async (req, res)=>{
+const postApp  = async (req, res)=>{
   const { name, desc, link} = req.body
   const {id} = req.user
   console.log(id)
   const app = new App({name, desc, link})
   app.user = id
-  app.save()
+  await app.save()
   
   res.json({
     msg:"app agregada",
@@ -17,6 +19,23 @@ postApp  =async (req, res)=>{
   })
 }
 
+const getAppsByUserId = async (req, res)=>{
+  const {id} = req.user
+  const apps = await App.find({ user: ObjectId(id)})
+
+  if(!apps){
+    return res.status(404).json({
+      msg: "no hay aplicaciones agregadas por este usuario"
+    })
+  }
+  else{
+    return res.json({
+      app: apps
+    })
+  }
+}
+
 module.exports = {
-  postApp
+  postApp,
+  getAppsByUserId
 }
